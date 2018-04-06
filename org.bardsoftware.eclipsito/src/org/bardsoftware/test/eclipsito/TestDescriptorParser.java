81c35a6567f850149cadc4014d0db2ecab724312
Dmitry Barashev <dbarashev@bardsoftware.com>
2018-04-06 16:29:03 +0300

    Fixed unit tests and added build.gradle

--- org.bardsoftware.eclipsito/src/org/bardsoftware/test/eclipsito/TestDescriptorParser.java
+++ org.bardsoftware.eclipsito/src/org/bardsoftware/test/eclipsito/TestDescriptorParser.java
@@ -1,23 +1,23 @@
 package org.bardsoftware.test.eclipsito;
 
-import java.net.URI;
-import java.security.InvalidParameterException;
-
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.FactoryConfigurationError;
-import javax.xml.parsers.ParserConfigurationException;
-
 import org.bardsoftware.impl.eclipsito.DescriptorParser;
 import org.bardsoftware.impl.eclipsito.PluginDescriptor;
 import org.eclipse.core.runtime.IModel;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.FactoryConfigurationError;
+import javax.xml.parsers.ParserConfigurationException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.security.InvalidParameterException;
+
 
 // sorry, this test is only a stub for now, i'm not sure it is wise to implement it now
 public class TestDescriptorParser extends TestsEclipsitoBase {
 
-    public void testSimpiestDescriptor() throws ParserConfigurationException, FactoryConfigurationError {
+    public void testSimpiestDescriptor() throws Exception, FactoryConfigurationError {
         String testAttr = "test";
         Element root = createPluginDocumentElement();
 		root.setAttribute(IModel.PLUGIN_ID, testAttr);
@@ -35,8 +35,8 @@ public class TestDescriptorParser extends TestsEclipsitoBase {
 		assertEquals(0, descriptor.getExtensionPointDescriptors().length);
 		assertEquals(0, descriptor.getExtensions().length);
     }
-    
-    public void testMissingRequiredPluginAttributes() throws ParserConfigurationException, FactoryConfigurationError {
+
+    public void testMissingRequiredPluginAttributes() throws Exception, FactoryConfigurationError {
         Element root = createPluginDocumentElement();
         try {
             DescriptorParserMock.test(root);
@@ -45,16 +45,16 @@ public class TestDescriptorParser extends TestsEclipsitoBase {
             assertTrue(true);
         }
     }
-    
+
     private Element createPluginDocumentElement() throws ParserConfigurationException, FactoryConfigurationError {
 		Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
 		return (Element) doc.appendChild(doc.createElement(IModel.PLUGIN));
     }
-    
+
     private static class DescriptorParserMock extends DescriptorParser {
-        public static PluginDescriptor test(Element pluginElement) {
+        public static PluginDescriptor test(Element pluginElement) throws MalformedURLException {
             URI pluginDescriptorUri = URI.create("");
-            return constructPluginDescriptor(pluginElement, pluginDescriptorUri);
+            return constructPluginDescriptor(pluginElement, pluginDescriptorUri.toURL());
         }
     }
 }
--- org.bardsoftware.eclipsito/src/org/bardsoftware/test/eclipsito/TestModulesDirectoryProcessor.java
+++ org.bardsoftware.eclipsito/src/org/bardsoftware/test/eclipsito/TestModulesDirectoryProcessor.java
@@ -1,41 +1,41 @@
 package org.bardsoftware.test.eclipsito;
 
+import org.bardsoftware.impl.eclipsito.ModulesDirectoryProcessor;
+import org.bardsoftware.impl.eclipsito.PluginDescriptor;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Random;
 
-import org.bardsoftware.impl.eclipsito.ModulesDirectoryProcessor;
-import org.bardsoftware.impl.eclipsito.PluginDescriptor;
-
 // please, add eclipsito home directory to the classpath before running this test!
 public class TestModulesDirectoryProcessor extends TestsEclipsitoBase {
-    
+
     public void testEmptyContent() {
         File tempDirectory = getTempDirectory();
         File[] contents = tempDirectory.listFiles();
         assertEquals(0, contents.length);
-        PluginDescriptor[] descriptors = ModulesDirectoryProcessor.process(tempDirectory.toURI(), "");
+        PluginDescriptor[] descriptors = ModulesDirectoryProcessor.process(tempDirectory, "");
         assertEquals(0, descriptors.length);
     }
-    
+
     public void testNonExistingModulesDirectory() {
         File ghostFile = new File(getTempDirectory(), "ghost");
         assertFalse(ghostFile.exists());
-        PluginDescriptor[] descriptors = ModulesDirectoryProcessor.process(ghostFile.toURI(), "*");
+        PluginDescriptor[] descriptors = ModulesDirectoryProcessor.process(ghostFile, "*");
         assertEquals(0, descriptors.length);
     }
-    
+
     public void descriptorInRoot() throws IOException {
         String descrPattern = "test.descriptor";
         File descriptor = new File(getTempDirectory(), descrPattern);
         descriptor.createNewFile();
         fillFileWithTestContent(descriptor);
         assertTrue(descriptor.exists());
-        PluginDescriptor[] descriptors = ModulesDirectoryProcessor.process(getTempDirectory().toURI(), descrPattern);
+        PluginDescriptor[] descriptors = ModulesDirectoryProcessor.process(getTempDirectory(), descrPattern);
         assertEquals(0, descriptors.length);
     }
-    
+
     public void testOneExistingModulesDescriptor() throws IOException {
         String descrPattern = "test.descriptor";
         File module = new File(getTempDirectory(), "test");
@@ -44,13 +44,13 @@ public class TestModulesDirectoryProcessor extends TestsEclipsitoBase {
         descriptor.createNewFile();
         fillFileWithTestContent(descriptor);
         assertTrue(descriptor.exists());
-        PluginDescriptor[] descriptors = ModulesDirectoryProcessor.process(getTempDirectory().toURI(), descrPattern);
+        PluginDescriptor[] descriptors = ModulesDirectoryProcessor.process(getTempDirectory(), descrPattern);
         assertNotNull(descriptors);
         assertEquals(1, descriptors.length);
         descriptor.delete();
         module.delete();
     }
-    
+
     public void testNExistingModulesDescriptors() throws IOException {
         int n = 3;
         String descrPattern = "test.descriptor";
@@ -64,20 +64,20 @@ public class TestModulesDirectoryProcessor extends TestsEclipsitoBase {
             moduleDescriptors[i].createNewFile();
             fillFileWithTestContent(moduleDescriptors[i]);
         }
-        PluginDescriptor[] descriptors = ModulesDirectoryProcessor.process(getTempDirectory().toURI(), descrPattern);
+        PluginDescriptor[] descriptors = ModulesDirectoryProcessor.process(getTempDirectory(), descrPattern);
         assertEquals(n, descriptors.length);
         for (int i = 0; i < n; i++) {
             moduleDescriptors[i].delete();
             moduleDirectories[i].delete();
         }
     }
-    
+
     private void fillFileWithTestContent(File descriptor) throws IOException {
         FileWriter writer = new FileWriter(descriptor);
         writer.write(TEST_CONTENT_0);
         writer.close();
     }
-    
+
     private File getTempDirectory() {
         File result = new File(getClass().getClassLoader().getResource("modules").getPath(), "test");
         result.mkdir();
